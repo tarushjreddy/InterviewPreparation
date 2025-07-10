28ff94c95356eff6ace9944f86d51f6416da1339
Tarush J.R <53051381+tarushjreddy@users.noreply.github.com>
2025-07-10 12:41:48 +0530

    Added insertion sort and selection sort

---
+++ insertionSort.java
@@ -0,0 +1,30 @@
+
+public class insertionSort {
+
+    public static void main(String[] args) {
+        int nums[] = {9, 6, 7, 8, 9};
+        int[] result = InsertionSort(nums);
+        System.out.println("Sorted Array using insertion sort");
+        for (int num : result) {
+            System.out.print(num);
+        }
+
+    }
+
+    public static int[] InsertionSort(int nums[]) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            System.out.println("Before while loop" + " value of i " + i + " Value of j" + j);
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                System.out.println("in while loop" + " value of i " + i + " Value of j " + j);
+                j--;
+            }
+            System.out.println("After while loop" + " value of i " + i + " Value of j " + j);
+            nums[j + 1] = key;
+        }
+        return nums;
+    }
+
+}
--- playground.java
+++ playground.java
@@ -9,43 +9,65 @@ public class playground {
         Scanner typemethod = new Scanner(System.in);
         System.out.println("Please enter you want to perform Sorting or Searching");
         String inputmethod = typemethod.nextLine();
-        typemethod.close();
- 
-       if("Searching".equals(inputmethod)){
-        Scanner typeInput = new Scanner(System.in);
-        String type = typeInput.nextLine();
-        typeInput.close();
-        int res = findSearching(nums, target, type);
-        if (res == -1) {
-            System.out.println("Element is not present");
-        } else {
-            System.out.println("Element is present in this position of the nums " + res + " where the value for nums is " + nums[res]);
-        }
-       }
-       else if ("Sorting".equals(inputmethod)){
-        System.out.println("Sorting Program Triggered");
-        int unsorted[] = {3, 4, 5, 6, 7, 8, 9, 10};
-        int res[] = findSorting(unsorted);
-       
-        for(int num : res){
-            System.out.print(num+" ");
-        }
-       }
-    }
-    public static int[] findSorting(int[] nums){
+        if ("Searching".equals(inputmethod)) {
+            Scanner typeInput = new Scanner(System.in);
+            String type = typeInput.nextLine();
+            typeInput.close();
+            int res = findSearching(nums, target, type);
+            if (res == -1) {
+                System.out.println("Element is not present");
+            } else {
+                System.out.println("Element is present in this position of the nums " + res + " where the value for nums is " + nums[res]);
+            }
+        } else if ("Sorting".equals(inputmethod)) {
+            System.out.println("Sorting Program Triggered");
+            System.out.println("Select the type of Sorting");
+          
+            String typeofSort = typemethod.nextLine();
 
-        for(int i=0; i < nums.length; i++){
-            System.out.println(nums[i]);
-            for(int j=0; j<nums.length-i-1; j++){
-                if(nums[j]>nums[j+1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] =temp;
+            int unsorted[] = {3, 4, 5, 6, 7, 8, 9, 10};
+            int res[] = findSorting(unsorted, typeofSort);
+
+            for (int num : res) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static int[] findSorting(int[] nums, String type) {
+        if ("Selection".equals(type)) {
+            int minIndex = 0;
+            for(int i =0; i<nums.length; i++){
+                minIndex = i;
+                for(int j=i+1; j<nums.length; j++){
+                    if(nums[minIndex]>nums[j]){
+                        minIndex = j;
+                    }
+                }
+                int temp = nums[minIndex];
+                nums[minIndex] = nums[i];
+                nums[i] = temp;
+            }
+            System.out.println("Sorted array using Selection Sort will be as follows");
+            for(int num : nums){
+                System.out.println(num);
+            }
+
+        } else if ("Bubble".equals(type)) {
+            for (int i = 0; i < nums.length; i++) {
+                System.out.println(nums[i]);
+                for (int j = 0; j < nums.length - i - 1; j++) {
+                    if (nums[j] > nums[j + 1]) {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
                 }
             }
         }
-    return nums;
+        return nums;
     }
+
     public static int findSearching(int[] nums, int target, String type) {
         int right = nums.length - 1;
         int left = 0;
